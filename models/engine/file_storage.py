6b09cb146e8bacb56dfa1b9fe07d7511cc2ac8f7
Emna <bhmemna7@gmail.com>
2020-03-31 22:48:04 +0000

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,18 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        obj_dict = {}
+        if cls is not None:
+            obj_dict = {key: value for key, value
+                    in self.__objects.items() if cls.__name in key}
+            return obj_dict
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +61,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes an object from __objects if inside
+        """
+        if obj in self.__objects.values():
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del(self.__objects[key])
