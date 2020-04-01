91ababed24d9bfa8f336ab4fb5ce536fab35ad87
Emna <bhmemna7@gmail.com>
2020-04-01 18:18:13 +0000

    task 7

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, String
+import os
 
 class User(BaseModel, Base):
     """This is the class for user
