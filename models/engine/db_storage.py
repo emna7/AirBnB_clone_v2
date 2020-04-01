ea824a5de45e8c631c8b52c94324a9c3ef15164b
atefsama666 <mechken.atef@gmail.com>
2020-04-01 22:18:35 +0100

    Mapped Place class to DB

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -22,7 +22,7 @@ class DBStorage():
     """
     __engine = None
     __session = None
-    all_classes = ["State", "City", "User"]
+    all_classes = ["State", "City", "User", "Place"]
 
     def __init__(self):
         """ Init method for DBStorage class """
